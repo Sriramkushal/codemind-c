d9a172d72da0b8c2b862ace68b34e77cd9c1f45a
Sriramkushal <98822628+Sriramkushal@users.noreply.github.com>
2022-02-21 21:03:16 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j==1||j==i||i==n)          printf("*");          else          {          printf(" ");         }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,13 +3,18 @@ int main()
 {
     int i,j,n;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
-        { 
-            printf("%d",j);
+        for(j=1;j<=n;j++)
+        {
+            if(j==1||j==i||i==n)
+         printf("*");
+         else
+         {
+         printf(" ");
         }
-       printf("
+        }
+        printf("
 ");
     }
 }
